82bf3116abedaa1002e72d332a1ea5818bcb43df
alagappanr <ralagappan2006@gmail.com>
2012-08-16 22:40:33 +0530

    Rearranging files

---
+++ practice_problems/abcd.py
@@ -0,0 +1,9 @@
+def sayHello():
+    print "HELLO"
+
+if __name__ == '__main__':
+    print 'This program is being run by itself'
+else:
+    print 'I am being imported from another module'
+						
+									
---
+++ practice_problems/xyz.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('/home/alagappan/python_problems')
+import abcd
+
+print "INSIDE XYZ"
+abcd.sayHello()
