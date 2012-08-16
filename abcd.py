908a6a83321e20beaed63078e67e4dca4cebdb37
alagappanr <ralagappan2006@gmail.com>
2012-08-16 23:02:40 +0530

    Removed some files from the base folder

--- abcd.py
+++
@@ -1,9 +0,0 @@
-def sayHello():
-    print "HELLO"
-
-if __name__ == '__main__':
-    print 'This program is being run by itself'
-else:
-    print 'I am being imported from another module'
-						
-									
--- xyz.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('/home/alagappan/python_problems')
-import abcd
-
-print "INSIDE XYZ"
-abcd.sayHello()
